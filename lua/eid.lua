88ca82ccf3babc1158cb3aacabc06d82a10ed7d7
anchikai <95461402+anchikai@users.noreply.github.com>
2021-12-21 02:27:35 -0500

    Fixed some Spanish translations
    Thank you for the help Kotry!

--- lua/eid.lua
+++ lua/eid.lua
@@ -4,23 +4,23 @@
 -- Ophiuchus
 EID:addCollectible(CollectibleType.COLLECTIBLE_OPHIUCHUS, "↑ +0.4 Tears up#↑ +0.3 Damage up#↑ +1 Soul Heart#Tears move in waves#Spectral tears", "Ophiuchus", "en_us")
 EID:addCollectible(CollectibleType.COLLECTIBLE_OPHIUCHUS, "↑ +0,4 к скорострельности#↑ +0,3 к урону#↑ +1 синее сердце#Слезы движутся волнами#Спектральные слезы", "Змееносец", "ru")
-EID:addCollectible(CollectibleType.COLLECTIBLE_OPHIUCHUS, "↑ +0.4 de lágrimas#↑ +0.3 de dañi#↑ +1 Corazón de almat#Las lágrimas se mueven en ondas#Lágrimas espectrales", "Ofiuco", "spa")
+EID:addCollectible(CollectibleType.COLLECTIBLE_OPHIUCHUS, "↑ +0.4 de lágrimas#↑ +0.3 de daño#↑#↑ +1 Corazón de almat#Las lágrimas se mueven en ondas#Lágrimas espectrales", "Ofiuco", "spa")
 
 -- Chiron
 EID:addCollectible(CollectibleType.COLLECTIBLE_CHIRON, "↑ +0.2 Speed up#Entering a new floor will give a random mapping effect#Entering a boss room will trigger a book effect", "Chiron", "en_us")
 EID:addCollectible(CollectibleType.COLLECTIBLE_CHIRON, "↑ +0.2 к скорости#Переходя на новый этаж даёт случайный эффект отображения на карте#Заходя в комнату с боссом активирует эффект книги", "Хирон", "ru")
-EID:addCollectible(CollectibleType.COLLECTIBLE_CHIRON, "↑ +0.2 de velocidad#Entrar a un nuevo piso dará un efecto de mapeado distinto#Entrar a una habitación del jefe dará un efecto de libro aleatorio", "Quirón", "spa")
+EID:addCollectible(CollectibleType.COLLECTIBLE_CHIRON, "↑ +0.2 de velocidad#Entrar a un nuevo piso dará un efecto de mapa aleatorio#Entrar a una habitación del jefe dará un efecto de libro aleatorio", "Quirón", "spa")
 EID:assignTransformation("collectible", CollectibleType.COLLECTIBLE_CHIRON, "5, 12")
 
 -- Juno
 EID:addCollectible(CollectibleType.COLLECTIBLE_JUNO, "↑ +1 Soul Heart#Tears that hit an enemy have a chance to chain down the nearest enemy for 5 seconds, preventing them from acting", "Juno", "en_us")
 EID:addCollectible(CollectibleType.COLLECTIBLE_JUNO, "↑ +1 синее сердце#Слезы, попавшие во врага, могут сковать в цепь ближайшего врага на 5 секунд, не позволяя ему двигаться", "Юнона", "ru")
-EID:addCollectible(CollectibleType.COLLECTIBLE_JUNO, "↑ +1 Corazón de alma#Las lágrimas quer golpeen a un enemigo tienen una posibilidad de encadenar al enemigo más cercano, evitando que actúe", "Juno", "spa")
+EID:addCollectible(CollectibleType.COLLECTIBLE_JUNO, "↑ +1 Corazón de alma#Las lágrimas que golpeen a un enemigo tienen una posibilidad de encadenar al enemigo más cercano, evitando que actúe", "Juno", "spa")
 
 -- Pallas
 EID:addCollectible(CollectibleType.COLLECTIBLE_PALLAS, "Tears bounce off the floor after floating for a short time#Tears cause splash damage on every bounce#Increases tear size by 20%", "Pallas", "en_us")
 EID:addCollectible(CollectibleType.COLLECTIBLE_PALLAS, "Слезы отскакивают от пола после непродолжительного полёта#Слезы наносят урон по площади при каждом отскоке#Увеличивает размер слезы на 20%", "Паллас", "ru")
-EID:addCollectible(CollectibleType.COLLECTIBLE_PALLAS, "Las lágrimas rebotan en el suelo después de flotar por un rato#Las l'grimas provocan daño de salpicadura al rebotar#El tamaño de lágrimas aumenta un 20%", "Pallas", "spa")
+EID:addCollectible(CollectibleType.COLLECTIBLE_PALLAS, "Las lágrimas rebotan en el suelo después de flotar por un rato#Las lágrimas provocan daño de salpicadura al rebotar#El tamaño de lágrimas aumenta un 20%", "Pallas", "spa")
 
 -- Ceres
 EID:addCollectible(CollectibleType.COLLECTIBLE_CERES, "↑ +0.5 Damage up#Tears that hit an enemy have a chance to cause the player to produce green creep#Enemies that walk over that creep will be slowed and a tentacle will attack them#The tentacle will also slow the enemy", "Ceres", "en_us")
