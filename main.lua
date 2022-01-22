c8074ac53c16cbf8e67424bbfc6dc4e769a1784e
anchikai <95461402+anchikai@users.noreply.github.com>
2022-01-22 17:03:29 -0500

    Detection for Furtherance

--- main.lua
+++ main.lua
@@ -9,12 +9,22 @@ CollectibleType.COLLECTIBLE_PALLAS = Isaac.GetItemIdByName("Pallas")
 CollectibleType.COLLECTIBLE_JUNO = Isaac.GetItemIdByName("Juno")
 CollectibleType.COLLECTIBLE_VESTA = Isaac.GetItemIdByName("Vesta")
 
-include("lua/items/Ophiuchus.lua")
-include("lua/items/Chiron.lua")
-include("lua/items/Ceres.lua")
-include("lua/items/Pallas.lua")
-include("lua/items/Juno.lua")
-include("lua/items/Vesta.lua")
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, function(_, isContinue)
+	if not further then
+		include("lua/items/Ophiuchus.lua")
+		include("lua/items/Chiron.lua")
+		include("lua/items/Ceres.lua")
+		include("lua/items/Pallas.lua")
+		include("lua/items/Juno.lua")
+		include("lua/items/Vesta.lua")
+	elseif not _Keyboard then
+		Isaac.RenderText("Furtherance is installed! Please disable Astrological Signs.", 60, 240, 1, 1, 1, 255)
+	else
+		Isaac.RenderText("Furtherance is installed! Please disable Astrological Signs", 60, 240, 1, 1, 1, 255)
+		Isaac.RenderText("and Isaac's Keyboard.", 60, 253, 1, 1, 1, 255)
+	end
+end
+)
 
 -- prevent shaders crash
 mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, function()
